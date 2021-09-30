44836dd6cdbec3cd7f72eef9582e89a9f7ac6296
perezsaura-david <perezsaura.david@gmail.com>
2021-09-30 15:12:36 +0200

    Pose and velocities working

---
+++ build/CMakeFiles/realsense_interface_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/realsense_interface_node.dir/src/rs_t265_interface.cpp.o"
+  "CMakeFiles/realsense_interface_node.dir/src/rs_t265_interface_node.cpp.o"
+  "realsense_interface_node"
+  "realsense_interface_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/realsense_interface_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dps/aerostack2_ws/src/realsense_interface
+# Build directory: /home/dps/aerostack2_ws/src/realsense_interface/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
