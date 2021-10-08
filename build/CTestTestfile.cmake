f67e6757005fe10a355b68088ca64f09ab673456
perezsaura-david <perezsaura.david@gmail.com>
2021-10-08 10:34:31 +0200

    Clean folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dps/aerostack2_ws/src/aerostack2/stack/hardware_interface/realsense_interface
-# Build directory: /home/dps/aerostack2_ws/src/aerostack2/stack/hardware_interface/realsense_interface/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
