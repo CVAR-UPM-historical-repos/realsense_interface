5e5e21a15e0774fc66619ee12a0852fb5c1f091e
perezsaura-david <perezsaura.david@gmail.com>
2021-11-25 15:52:20 +0100

    Documentation

--- include/realsense_interface/rs_t265_interface.hpp
+++ include/realsense_interface/rs_t265_interface.hpp
@@ -1,3 +1,35 @@
+/*!*******************************************************************************************
+ *  \file       rs_t265_interface.hpp
+ *  \brief      Realsense t265 tracking camera interface header file.
+ *  \authors    David Perez Saura
+ *  \copyright  Copyright (c) 2021 Universidad Politécnica de Madrid
+ *              All Rights Reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************************/
+
 #ifndef RS_T265_INTERFACE_HPP_
 #define RS_T265_INTERFACE_HPP_
 
@@ -28,13 +60,32 @@
 #include <geometry_msgs/msg/transform_stamped.hpp>
 #include <geometry_msgs/msg/vector3.hpp>
 
-
+/**
+ * @brief Check the stream type of the device
+ * 
+ * @param stream_requests 
+ * @param out_serial 
+ * @return true  Vio device found
+ * @return false Vio device not found
+ */
 bool find_device_with_stream(std::vector <rs2_stream> stream_requests, std::string& out_serial);
+
+/**
+ * @brief Check if the device provides imu information
+
+ * @return true  Device provides IMU information
+ * @return false Device doesn't provide IMU information
+ */
 bool check_imu_is_supported();
 
 class RsT265Interface:public as2::Node{
     public:
+        /**
+         * @brief Constructor of the RsT265Interface object
+         * 
+         */
         RsT265Interface();
+
         // void setup_imu();
         // void run_imu();
         // void ownSetup(){
@@ -45,16 +96,46 @@ class RsT265Interface:public as2::Node{
         //     run_odom();
         //     run_tf();
         // }
+
+        /**
+         * @brief Initial setup for node odometry.
+         * This function check if the VIO device is ready and starts the pipeline.
+         */
         void setupOdom();
+
+        /**
+         * @brief Stop rutine for odometry node.
+         * This function stops the pipeline.
+         */
         void stopOdom();
+
+        /**
+         * @brief Funtionality during node lifetime.
+         * This function gets the pose data from VIO device,
+         * updates the transform tree and
+         * and publish the odometry message.
+         */
         void runOdom();
-
-        void setupTf();
-        void publishTFs();
-        void setTfTree();
         
-        void publishEstimatedPose();
+        /**
+         * @brief Initial setup for node Tranforms
+         * This function generate the Transforms tree
+         * from the relative position between links.
+         */
+        void setupTf();
 
+        /**
+         * @brief Publish Transforms tree
+         * This function publish the Transformation
+         * between base link and device link
+         */
+        void publishTFs();
+
+        /**
+         * @brief Set the Tf Tree object
+         * This function updates the Tranform tree
+         */
+        void setTfTree();
 
     private:
         // Sensor comm
